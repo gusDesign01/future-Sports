d34ff719c9b92219fd0123c17cd7d551aff16956
gusDesign01 <gustavo_ordonez@live.com.au>
2023-09-18 20:25:06 +1000

    test Branch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Aside from './Components/Aside/Aside';
+//import Aside from './Components/Aside/Aside';
 import Footer from './Components/Footer/Footer';
-import Main from './Components/Main/Main';
+//import Main from './Components/Main/Main';
 import Navigation from './Components/Navigation/Navigation';
 import Header from './Components/Header/Header'
-//import ContactUs from './Components/Aside/ContactUs';
+
 
 
 function App() {
